52ab7255bb3252d910a77ab6f2f2d2f88678082a
Alexej Hamburg <alexej.hamburg1@gmail.com>
2024-07-18 19:13:26 +0200

    Add metrics count.

--- main.go
+++ main.go
@@ -4,7 +4,34 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Print("Intercepting ", cfg.fileserverHits)
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	}) 
+}
+
+func (cfg *apiConfig) metrics() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
+		response := "Hits: " + strconv.Itoa(cfg.fileserverHits)
+		io.WriteString(w, response)
+	}) 
+}
+
+func (cfg *apiConfig) reset() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits = 0
+	}) 
+}
 
 func main() {
     // Use the http.NewServeMux() function to create an empty servemux.
@@ -12,14 +39,17 @@ func main() {
 
 	log.Print("Listening...")
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./public"))))
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
 		io.WriteString(w, "OK")
 	})
+	
+	fileHandle := http.StripPrefix("/app/", http.FileServer(http.Dir("./public")))
+	apiCfg := apiConfig{0};
+	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(fileHandle))
+	mux.Handle("/metrics", apiCfg.metrics())
+	mux.Handle("/reset", apiCfg.reset())
 
-	// Then we create a new server and start listening for incoming requests
-	// with the http.ListenAndServe() function, passing in our servemux for it to
-	// match requests against as the second argument.
+
 	http.ListenAndServe(":8080", mux)
 }
