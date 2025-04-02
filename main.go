caf034394c26772682c52a1acdb84336318c6dc0
Alexej Hamburg <alexej.hamburg1@gmail.com>
2025-04-02 23:21:55 +0200

    Setup gin example.

--- main.go
+++ main.go
@@ -1,55 +1,34 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
-	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
-type apiConfig struct {
-	fileserverHits int
+// album represents data about a record album.
+type album struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Print("Intercepting ", cfg.fileserverHits)
-		cfg.fileserverHits++
-		next.ServeHTTP(w, r)
-	}) 
+// albums slice to seed record album data.
+var albums = []album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-func (cfg *apiConfig) metrics() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
-		response := "Hits: " + strconv.Itoa(cfg.fileserverHits)
-		io.WriteString(w, response)
-	}) 
-}
-
-func (cfg *apiConfig) reset() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits = 0
-	}) 
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, albums)
 }
 
 func main() {
-    // Use the http.NewServeMux() function to create an empty servemux.
-	mux := http.NewServeMux()
+    router := gin.Default()
+    router.GET("/albums", getAlbums)
 
-	log.Print("Listening...")
-
-	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type:", "text/plain; charset=utf-8")
-		io.WriteString(w, "OK")
-	})
-	
-	fileHandle := http.StripPrefix("/app/", http.FileServer(http.Dir("./public")))
-	apiCfg := apiConfig{0};
-	mux.Handle("/app/*", apiCfg.middlewareMetricsInc(fileHandle))
-	mux.Handle("POST /metrics", apiCfg.metrics())
-	mux.Handle("DELETE /reset", apiCfg.reset())
-
-
-	http.ListenAndServe(":8080", mux)
+    router.Run("localhost:8080")
 }
