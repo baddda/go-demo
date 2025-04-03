5b0a59d1faac520f6595954cf0e3373aad4a5e00
Alexej Hamburg <alexej.hamburg1@gmail.com>
2025-04-03 09:19:28 +0200

    Rename albums to tasks.

--- main.go
+++ main.go
@@ -6,41 +6,37 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+type task struct {
+	ID          string `json:"id"`
+	Description string `json:"description"`
 }
 
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+var tasks = []task{
+	{ID: "1", Description: "Blue Train"},
+	{ID: "2", Description: "Jeru"},
+	{ID: "3", Description: "Sarah Vaughan and Clifford Brown"},
 }
 
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+func getTasks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, tasks)
 }
 
-func postAlbums(c *gin.Context) {
-	var newAlbum album
+func postTasks(c *gin.Context) {
+	var newTask task
 
-	// Bind the received JSON to newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
+	if err := c.BindJSON(&newTask); err != nil {
 		return
 	}
 
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	tasks = append(tasks, newTask)
+	c.IndentedJSON(http.StatusCreated, newTask)
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
+	router.GET("/tasks", getTasks)
 
-	router.POST("/albums", postAlbums)
+	router.POST("/tasks", postTasks)
 
 	router.Run("localhost:8080")
 }
