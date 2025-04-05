290674373190e22a4ef8acc89c05023a2d9e71f3
Alexej Hamburg <alexej.hamburg1@gmail.com>
2025-04-05 09:10:40 +0200

    Add post

--- test/controller_test.go
+++ test/controller_test.go
@@ -3,6 +3,7 @@ package controller_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"tasko/internal/controller"
 	"testing"
 
@@ -10,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetUserByID(t *testing.T) {
+func TestGetTasks(t *testing.T) {
 	router := gin.Default()
 	router.GET("/api/tasks", controller.GetTasks)
 
@@ -22,4 +23,19 @@ func TestGetUserByID(t *testing.T) {
 
 	assert.Equal(t, http.StatusOK, rec.Code)
 	assert.Equal(t, "[\n    {\n        \"id\": \"1\",\n        \"description\": \"Buy bread\"\n    },\n    {\n        \"id\": \"2\",\n        \"description\": \"Toast bread\"\n    },\n    {\n        \"id\": \"3\",\n        \"description\": \"Eat bread\"\n    }\n]", rec.Body.String())
+}
+func TestPostTask(t *testing.T) {
+	router := gin.Default()
+	router.POST("/api/tasks", controller.PostTask)
+
+	newTask := `{"description": "New task"}`
+
+	req, err := http.NewRequest("POST", "/api/tasks", strings.NewReader(newTask))
+	assert.NoError(t, err)
+
+	rec := httptest.NewRecorder()
+	router.ServeHTTP(rec, req)
+
+	assert.Equal(t, http.StatusCreated, rec.Code)
+	assert.Equal(t, "{\n    \"id\": \"\",\n    \"description\": \"New task\"\n}", rec.Body.String())
 }
