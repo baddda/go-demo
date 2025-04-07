fbb40402c0d26a9ffc19204bef9d9b54a9e78323
Alexej Hamburg <alexej.hamburg1@gmail.com>
2025-04-07 08:07:37 +0200

    Add integration test for server run. Rename package in controller_test.

---
+++ test/main_test.go
@@ -0,0 +1,24 @@
+package test
+
+import (
+	"net/http"
+	"tasko/internal/router"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServerRun(t *testing.T) {
+	router := router.SetupRouter()
+
+	go func() {
+		err := router.Run(":8080")
+		assert.NoError(t, err)
+	}()
+
+	resp, err := http.Get("http://localhost:8080/api/tasks")
+	assert.NoError(t, err)
+	defer resp.Body.Close()
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+}
